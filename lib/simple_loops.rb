37586d46f7ea98a5119573ec8e625ac6cc3f0b74
calm-collection-3916 <khalinredd@gmail.com>
2020-12-29 17:28:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,6 +28,11 @@ output_array(["hello", "no", "yes", "maybe"])
 def return_string_array(array)
   count = 0 
   while count < array.length do
+    array[count].to_s
+    count += 1 
+  end
+end
+return_string_array("0")
     
   
   
