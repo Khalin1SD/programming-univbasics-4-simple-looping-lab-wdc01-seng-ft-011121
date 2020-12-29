0d02ec5c56d0f19f8df26bea7a5601d259c92150
calm-collection-3916 <khalinredd@gmail.com>
2020-12-29 17:24:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,49 @@
-# Write your methods here
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1 
+  end
+end
+loop_message_five_times("Hello World")
+
+def loop_message_n_times(message, limit)
+  count = 0 
+  while count < limit do 
+    puts message
+    count += 1 
+  end
+end
+loop_message_n_times("Hello Moon", 5)
+
+def output_array(array)
+  count = 0 
+  while count < array.length do 
+    puts array[count]
+    count += 1 
+  end
+end
+output_array(["hello", "no", "yes", "maybe"])
+
+def return_string_array(array)
+  count = 0 
+  while count < array.length do 
+    
+  end
+end
+return_string_array("0")
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
